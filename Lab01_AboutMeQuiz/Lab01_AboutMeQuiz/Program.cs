7e00ea004d8cf6dac962b148f9cfcdfc47637210
Michael Kelly <mike.s.kelly@icloud.com>
2019-01-08 09:10:16 -0800

    Created the first question

--- Lab01_AboutMeQuiz/Lab01_AboutMeQuiz/Program.cs
+++ Lab01_AboutMeQuiz/Lab01_AboutMeQuiz/Program.cs
@@ -6,7 +6,35 @@ namespace Lab01_AboutMeQuiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("The following question are to see how well you know me.");
+            Console.WriteLine("Please answer the following 5 questions.");
+            Military();
+
         }
+
+        static int Military() //function for the first question
+        {
+            Console.WriteLine("How many years did I Serve in the U.S. Military?");
+            string questOne = Console.ReadLine();
+            int years = Convert.ToInt32(questOne);
+
+            if (years == 11)
+            {
+                Console.WriteLine("That is right, I served for 2 weeks shy of 11 years."); //displays this if the user answers correctly
+            }
+            else
+            {
+                Console.WriteLine("No, I had served for 2 weeks shy of 11 years.");
+            }
+
+        }
+
+        static string Car()
+        {
+
+        }
+
+
     }
 }
