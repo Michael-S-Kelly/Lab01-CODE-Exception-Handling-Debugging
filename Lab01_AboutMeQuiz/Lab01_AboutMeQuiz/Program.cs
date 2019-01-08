918ee7600cb12d485e2e1661af4fd17eea2b8f57
Michael Kelly <mike.s.kelly@icloud.com>
2019-01-08 09:54:53 -0800

    Created third question

--- Lab01_AboutMeQuiz/Lab01_AboutMeQuiz/Program.cs
+++ Lab01_AboutMeQuiz/Lab01_AboutMeQuiz/Program.cs
@@ -4,12 +4,16 @@ namespace Lab01_AboutMeQuiz
 {
     class Program
     {
+        int count = 0;
+
         static void Main(string[] args)
         {
             Console.Clear();
             Console.WriteLine("The following question are to see how well you know me.");
             Console.WriteLine("Please answer the following 5 questions.");
             Military();
+            Car();
+            BirthPlace();
 
         }
 
@@ -25,16 +29,47 @@ namespace Lab01_AboutMeQuiz
             }
             else
             {
-                Console.WriteLine("No, I had served for 2 weeks shy of 11 years.");
+                Console.WriteLine("No, I had served for 2 weeks shy of 11 years."); //displays this if the user answers incorrectly
             }
+            return years;
 
         }
 
-        static string Car()
+
+        static string Car() //function for the second question
         {
+            Console.WriteLine("What was the Make of my first car?");
+            string questTwo = Console.ReadLine();
 
+            if (questTwo == "Volvo")
+            {
+                Console.WriteLine("That is right, my first car was a Volvo 244GL"); //displays this if the user answers correctly
+                return questTwo;
+            } else
+            {
+                Console.WriteLine("Sorry, no, my first car was a Volvo 244GL"); //displays this if the user answers incorrectly
+                return questTwo;
+            }
+            
         }
 
+        static string BirthPlace() //function for the third question
+        {
+            Console.WriteLine("Which city was I born in?");
+            string questThree = Console.ReadLine();
+            string city = questThree.ToUpper();
+
+            if (city == "BURIEN")
+            {
+                Console.WriteLine("That is right, I was born in Burien, Washington."); //displays this if the user answers correctly
+                return questThree;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, no, I was born in Burien, Washington."); //displays this if the user answers incorrectly
+                return questThree;
+            }
+        }
 
     }
 }
